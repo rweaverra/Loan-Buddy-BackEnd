9ed9693b4944deb390b31536542661c0eaa1e80d
Ryan Weaver <rweaverra@gmail.com>
2022-08-12 16:37:55 -0600

    before changing db from sqlite

--- Loan-Buddy-Api/Program.cs
+++ Loan-Buddy-Api/Program.cs
@@ -2,6 +2,17 @@ using Loan_Buddy_Api;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("MyAllowedOrigins",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:3000", "http://localhost:3001") // note the port is included 
+                .AllowAnyHeader()
+                .AllowAnyMethod();
+        });
+});
+
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddControllers();
@@ -9,6 +20,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+app.UseCors("MyAllowedOrigins");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -20,7 +32,7 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.MapControllers();
 
-//var test = await UsersRepository.GetUsers();
-//Console.WriteLine("test" + test);
+var test = await UsersRepository.GetUsers();
+Console.WriteLine("test" + test);
 
 app.Run();
